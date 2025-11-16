5454a9ba5ae575c3bbe077baa42f9607a01cf2fc
pupungeggang2 <kyongkyong2892@gmail.com>
2025-11-16 21:00:09 +0900

    Start

---
+++ test/main.c
@@ -0,0 +1,34 @@
+#include <GLFW/glfw3.h>
+#include <GL/gl.h>
+#include <stdio.h>
+
+int main() {
+    GLFWwindow *window;
+    if (!glfwInit()) {
+        printf("Cannot Init GLFW!\n");
+        return -1;
+    }
+    glfwWindowHint(GLFW_SCALE_TO_MONITOR, GL_TRUE);
+    window = glfwCreateWindow(1280, 720, "OpenGL Test Window", NULL, NULL);
+    if (!window) {
+        printf("Window creation failed!\n");
+    }
+    glfwMakeContextCurrent(window);
+    printf("OpenGL Version: %s", glGetString(GL_VERSION));
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glBegin(GL_TRIANGLES);
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glVertex2f(0.0f, 0.5f);
+        glColor3f(0.0f, 1.0f, 0.0f);
+        glVertex2f(-0.5f, -0.5f);
+        glColor3f(0.0f, 0.0f, 1.0f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate();
+    return 0;
+}
