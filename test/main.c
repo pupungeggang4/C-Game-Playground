b933e48d9442cb8501fa9d11531acd3738791101
pupungeggang2 <kyongkyong2892@gmail.com>
2025-11-17 14:43:35 +0900

    VSync

--- test/main.c
+++ test/main.c
@@ -14,6 +14,7 @@ int main() {
         printf("Window creation failed!\n");
     }
     glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
     printf("OpenGL Version: %s", glGetString(GL_VERSION));
     while (!glfwWindowShouldClose(window)) {
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
