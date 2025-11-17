d22faa10bd9e762d942ce393954dec473ec29d2b
pupungeggang2 <kyongkyong2892@gmail.com>
2025-11-18 00:46:08 +0900

    Temp

--- darkblendtest/main.c
+++ darkblendtest/main.c
@@ -1,6 +1,7 @@
 #include <GLFW/glfw3.h>
 #include <GL/gl.h>
 #include <stdio.h>
+#include <math.h>
 
 #define LEFT 0
 #define RIGHT 1
@@ -8,6 +9,7 @@
 #define DOWN 3
 
 void renderRect(float[], float[]);
+void renderCircle(float[], float, int);
 void keyHandle(GLFWwindow*, int, int, int, int);
 void move();
 
@@ -31,33 +33,33 @@ int main() {
 
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
-    printf("OpenGL Version: %s", glGetString(GL_VERSION));
+    printf("OpenGL Version: %s\n", glGetString(GL_VERSION));
 
     while (!glfwWindowShouldClose(window)) {
         move();
         glClearColor(1.0, 1.0, 1.0, 1.0);
-        glClear(GL_COLOR_BUFFER_BIT);
-        glDisable(GL_BLEND);
-        renderRect((float[]){0.5, -0.5, 0.2, 0.2}, (float[]){0.0, 1.0, 0.0});
-        renderRect((float[]){-0.5, -0.5, 0.2, 0.2}, (float[]){1.0, 1.0, 0.0});
-        renderRect((float[]){0.5, 0.5, 0.2, 0.2}, (float[]){1.0, 0.0, 1.0});
-        renderRect((float[]){-0.5, 0.5, 0.2, 0.2}, (float[]){0.0, 1.0, 1.0});
-        renderRect((float[]){pos[0], pos[1], 0.2, 0.2}, (float[]){0.0, 0.0, 0.0});
-
+        glClear(GL_COLOR_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
         glEnable(GL_BLEND);
-        glBlendFunc(GL_DST_COLOR, GL_ONE_MINUS_SRC_ALPHA);
-        glBegin(GL_QUADS);
-        glColor4f(0.0, 0.0, 0.0, 1.0);
-        glVertex2f(-1.0, -1.0);
-        glVertex2f(1.0, -1.0);
-        glVertex2f(1.0, 1.0);
-        glVertex2f(-1.0, 1.0);
-        glColor4f(0.0, 1.0, 0.0, 1.0);
-        glVertex2f(pos[0] - 0.3, pos[1] - 0.3);
-        glVertex2f(pos[0] + 0.3, pos[1] - 0.3);
-        glVertex2f(pos[0] + 0.3, pos[1] + 0.3);
-        glVertex2f(pos[0] - 0.3, pos[1] + 0.3);
-        glEnd();
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        glEnable();
+        renderCircle(pos, 0.4, 40);
+
+        /*
+        glEnable(GL_STENCIL_TEST);
+        glClearStencil(0);
+        glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE);
+        glStencilFunc(GL_ALWAYS, 1, ~0);
+        glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
+        renderCircle(pos, 0.4, 40);
+
+        glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
+        glDepthMask(GL_TRUE);
+        glStencilFunc(GL_NOTEQUAL, 1, ~0);
+        glStencilOp(GL_KEEP, GL_KEEP, GL_KEEP);
+        renderRect((float[]){0.0, 0.0, 2.0, 2.0}, (float[]){0.0, 0.0, 0.0});
+        glDisable(GL_STENCIL_TEST);
+        */
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
@@ -119,4 +121,33 @@ void renderRect(float rect[4], float color[3]) {
     glVertex2f(rect[0] + rect[2] / 2, rect[1] - rect[3] / 2);
     glVertex2f(rect[0] + rect[2] / 2, rect[1] + rect[3] / 2);
     glEnd();
+}
+
+void renderCircle(float center[2], float radius, int nVertex) {
+    for (int i = 0; i < nVertex; i++) {
+        float angle1 = M_PI * 2.0 / nVertex * i, angle2 = M_PI * 2.0 / nVertex * (i + 1);
+        float vertex[10] = {
+            center[0], center[1],
+            center[0] + radius * cos(angle1) / 2.0, center[1] + radius * sin(angle1) / 2.0,
+            center[0] + radius * cos(angle1), center[1] + radius * sin(angle1),
+            center[0] + radius * cos(angle2) / 2.0, center[1] + radius * sin(angle2) / 2.0,
+            center[0] + radius * cos(angle2), center[1] + radius * sin(angle2),
+        };
+        glBegin(GL_TRIANGLES);
+        glColor4f(0.0, 0.0, 1.0, 1.0);
+        glVertex2f(vertex[0], vertex[1]);
+        glVertex2f(vertex[2], vertex[3]);
+        glVertex2f(vertex[6], vertex[7]);
+        glEnd();
+        glBegin(GL_QUADS);
+        glColor4f(0.0, 0.0, 1.0, 1.0);
+        glVertex2f(vertex[2], vertex[3]);
+        glColor4f(0.0, 1.0, 1.0, 0.5);
+        glVertex2f(vertex[4], vertex[5]);
+        glColor4f(0.0, 1.0, 1.0, 0.5);
+        glVertex2f(vertex[8], vertex[9]);
+        glColor4f(0.0, 0.0, 1.0, 1.0);
+        glVertex2f(vertex[6], vertex[7]);
+        glEnd();
+    }
 }
